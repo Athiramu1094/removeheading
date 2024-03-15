2aaddc10d4642dd04cafa44842d340e9a31f8f2a
Athiramu1094 <163101900+Athiramu1094@users.noreply.github.com>
2024-03-15 11:10:40 +0530

    Add files via upload
    A simple HTML file with JavaScript functionality to remove an h1 heading when a button is clicked.

---
+++ index.js
@@ -0,0 +1,16 @@
+const myButton = document.getElementById('button')
+const myH1 = document.getElementById('h1')
+
+myButton.addEventListener('click', function(){
+    
+    myH1.remove()
+     
+})
+
+
+
+
+
+   
+
+
